972a19bd92589e08b813554859089d4610e92ea5
Ali Samir <alisamir.eng@gmail.com>
2023-09-18 00:30:43 +0300

    1st commit

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import "./globals.css";
+import { Inter } from "next/font/google";
+import Navbar from "./components/Navbar/Navbar";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "GitHub: Let’s build from here",
+  description:
+    "GitHub is where over 100 million developers shape the future of software, together. Contribute to the open source community, manage your Git repositories, review code like a pro, track bugs and features, power your CI/CD and DevOps workflows, and secure code before you commit it.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        {/* <Navbar/> */}
+        {children}
+      </body>
+    </html>
+  );
+}
